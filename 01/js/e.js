59cfbff54b5576b64b1ce89c8f4fcf002fcaeaab
N3TO0 <idervaljose00@gmail.com>
2024-11-06 15:21:10 -0300

    segundo commit

---
+++ 01/js/e.js
@@ -0,0 +1,26 @@
+//comando primarios
+
+//alert("olá mundo");
+//console.log("olá mundo!");
+
+//var/let/const 
+//int
+//const primeiroValor = 10;
+//const segundoValor = 20;
+//console.log("primeiroVvalor + segundoValor: ", primeiroValor + segundoValor)
+
+//const valorTotal = primeiroValor+segundoValor;
+//console.log("primeiroVvalor + segundoValor: ",valorTotal);
+
+//str
+//const comunidade = "one piece";
+//const comunidade = 'one piece';
+//const comunidade = `one piece`;
+//console.log("comunidade:",comunidade);
+//console.log(`comunidade: ${comunidade}`);
+
+
+//Padrão camelCase para declarar variaveis
+//const autorDoCurso ="Iderval Jose"
+//Padrão snake_case
+//const autor_do_curso="Iderval Jose"
