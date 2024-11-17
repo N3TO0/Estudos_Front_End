648cc0bf65edb9cebd43cd0852db8e765819a2a7
Neto <idervaljose00@gmail.com>
2024-11-17 06:49:20 -0300

    alterações ex 1 pasta 1

--- 01/js/Ex_3.js
+++ 01/js/Ex_3.js
@@ -1,5 +1,5 @@
-// Condicional if else
-// 
+// // Condicional if else
+
 // const idade = 16;
 // const maioridade = 18;
 
@@ -15,7 +15,7 @@
 // }
 // console.log("---------------------------")
 
-// Operadores ternarios
+// // Operadores ternarios
 
 // idade >= maioridade
 // ? console.log("Neto pode dirigir")
@@ -23,34 +23,44 @@
 
 // console.log("---------------------------")
 
-//switch case
+// // switch case
 
-const dia_da_semana = 4;
-let nome_do_dia = undefined;
+// const dia_da_semana = 4;
+// let nome_do_dia = undefined;
 
-switch (dia_da_semana) {
-    case 1:
-        nome_do_dia = 'domingo';
-        break;
-    case 2:
-        nome_do_dia = 'segunda';
-        break;
-    case 3:
-        nome_do_dia = 'terça';
-        break;
-    case 4:
-        nome_do_dia = 'quarta';
-        break;
-    case 5:
-        nome_do_dia = 'quinta';
-        break;
-    case 6:
-        nome_do_dia = 'sexta';
-        break;
-    case 7:
-        nome_do_dia = 'sabado';
-        break;
-}
+// switch (dia_da_semana) {
+// case 1:
+// nome_do_dia = 'domingo';
+// break;
+// case 2:
+// nome_do_dia = 'segunda';
+// break;
+// case 3:
+// nome_do_dia = 'terça';
+// break;
+// case 4:
+// nome_do_dia = 'quarta';
+// break;
+// case 5:
+// nome_do_dia = 'quinta';
+// break;
+// case 6:
+// nome_do_dia = 'sexta';
+// break;
+// case 7:
+// nome_do_dia = 'sabado';
+// break;
+// }
 
-console.log(nome_do_dia);
+// console.log(nome_do_dia);
 
+let lista = ["neto", "luis", "joão", 22, 34, true, null, undefined]
+
+console.log(lista[1])
+lista[1] = 2
+
+console.log(lista[1])
+lista = ['a', 'q', 'w', 'z', 't']
+
+console.log(lista[4])
+console.log(lista.length) // informa quantos indices tem a lista 
