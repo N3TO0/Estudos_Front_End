27e8a8896ea0ec9766e585b9e9ca863da205a2db
neto_desktop <idervaljose00@gmail.com>
2025-08-12 19:17:13 -0300

    add file formulario and formatos

---
+++ Formatos/cod.js
@@ -0,0 +1,20 @@
+// For normal ---------------------------------------
+
+// for (let i=0; i <=10; i++){
+//     console.log(`numero: ${i}`);
+// }
+// console.log(`fim`);
+
+
+// const lista_objetos = [{ nome: "neto", nota: 10 },{nome: "ingrid", nota: 8},{nome: "lola", nota: 9}];
+
+// metodo ForEach ------------------------------------
+
+// lista_objetos.forEach((aluno) => {
+//     if ( aluno.nota > 6){
+//         console.log(`${aluno.nome} passou com nota: ${aluno.nota}`);
+//     }else{
+//         console.log(`${aluno.nome} reprovou com nota: ${aluno.nota}`);
+//     }
+// })
+// console.log("fim");
